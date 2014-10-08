8f7e9aec02669b328b465ed96be29fae1a99f01b
Gadi Cohen <dragon@wastelands.net>
2014-10-08 14:35:44 +0200

    update for meteor 0.9.3+

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-	summary: 'jQuery 2.x (2.1.0) that co-exists with jquery package (v1)'
+  summary: 'jQuery 2.x (2.1.0) that co-exists with jquery package (v1)',
+  version: "2.1.0_4",
+  git: "https://github.com/gadicc/meteor-jquery2.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
 	api.add_files('lib/jquery-2.1.0.js', 'client');
 	api.add_files('jquery2.js', 'client');
 	api.export(['jQuery2', '$2'], 'client'); 
